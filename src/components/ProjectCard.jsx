08447aa041ffa6b03b6c363929558c6d4480ffc4
Prem <prempravinsapkale@gmail.com>
2024-05-29 18:54:24 +0530

    prettified projects component

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,3 +1,29 @@
-export const ProjectCard = () => {
-   return;
+export const ProjectCard = ({ projectData, serial }) => {
+   if (serial < 10) {
+      serial = "0" + serial;
+   }
+
+   return (
+      <div className="w-full h-[90vh] flex items-center justify-between p-[80px]">
+         <div className="w-[40%] h-full grid place-content-center relative group">
+            <h1 className="text-6xl text-sky-500 font-bold absolute top-2 -left-6 z-[-2] group-hover:text-orange-500 transition-colors duration-300">
+               {serial}
+            </h1>
+            <div className="rounded-lg">
+               <img
+                  src={projectData.imgSrc}
+                  alt={projectData.title}
+                  className="w-[600px] h-[300px] object-cover rounded-3xl shadow-lg"
+               />
+               <div className="fadeBottom h-[5rem] relative bottom-[80px] right-0 rounded-b-3xl" />
+            </div>
+            <div className="text-[60px] scale-y-110 scale-x-110 text-sky-500 font-bold absolute bottom-[100px] -right-10 group-hover:text-orange-500 transition-colors duration-200 ease-in-out">
+               {projectData.title}
+            </div>
+         </div>
+         <div className="w-[60%] h-full grid place-content-center px-[100px] text-right text-xl text-[#111]">
+            {projectData.description}
+         </div>
+      </div>
+   );
 };
--- src/components/ProjectsElement.jsx
+++ src/components/ProjectsElement.jsx
@@ -3,9 +3,39 @@ import { ProjectCard } from "./ProjectCard";
 
 export const ProjectsElement = () => {
    return (
-      <div>
-         <ProjectCard />
+      <div className="mt-[101px]">
+         <div className="flex flex-col gap-[]">
+            {projects.map((project, idx) => (
+               <a href={project.link} target="_blank" className="z-[999]">
+                  <ProjectCard projectData={project} serial={idx + 1} />
+               </a>
+            ))}
+         </div>
          <ContactElement />
       </div>
    );
 };
+
+const projects = [
+   {
+      title: "Homely",
+      link: "https://homely-jet.vercel.app",
+      description:
+         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit quibusdam est deleniti, et accusantium suscipit deserunt non eos exercitationem vitae, rerum labore quaerat nulla dolore, quisquam provident sequi. Cumque magnam doloribus tempore temporibus architecto.",
+      imgSrc: "/img1-1.jpg",
+   },
+   {
+      title: "Intercord",
+      link: "https://intercord.vercel.app",
+      description:
+         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit quibusdam est deleniti, et accusantium suscipit deserunt non eos exercitationem vitae, rerum labore quaerat nulla dolore, quisquam provident sequi. Cumque magnam doloribus tempore temporibus architecto.",
+      imgSrc: "/img2-2.jpg",
+   },
+   {
+      title: "Hunger Bites",
+      link: "https://hunger-bites.vercel.app",
+      description:
+         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit quibusdam est deleniti, et accusantium suscipit deserunt non eos exercitationem vitae, rerum labore quaerat nulla dolore, quisquam provident sequi. Cumque magnam doloribus tempore temporibus architecto.",
+      imgSrc: "/img3-3.jpg",
+   },
+];
